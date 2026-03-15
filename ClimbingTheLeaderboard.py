420d75e5277d5b656699d2054bec233220b1bc93
Vinayparmar2264 <vinayparmar2264@gmail.com>
2026-03-16 00:11:27 +0530

    Update ClimbingTheLeaderboard.py

--- ClimbingTheLeaderboard.py
+++ ClimbingTheLeaderboard.py
@@ -1,1 +1,12 @@
+ranked = [100,100,50,40,40,20,10]
+player = [5,25,50,120]
 
+ranked = sorted(set(ranked))
+r = len(ranked)
+result =[]
+i = 0
+for score in player:
+  while(i<r and ranked[i]<=socre):
+    i+=1
+  result.append(r-i+1)
+return result
